01d94ac11e688e37c9121769891a35a4d9f11e55
adamikucz <adamikucz@int.pl>
2026-04-07 15:07:16 +0200

    Update news.js

--- api/news.js
+++ api/news.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
     });
 
     res.setHeader('Access-Control-Allow-Origin', '*');
+    news.reverse();
     res.status(200).json(news.slice(0, 5));
   } catch (err) {
     res.status(500).json({
